809d78b7035a7f30a3fc32e6731b20d3c5dc866c
Lina Silva <linaf.silvaz@gmail.com>
2024-08-20 22:18:45 -0600

    Deposit method included

---
+++ ConsoleBankApp/ExchangeRate.cs
@@ -0,0 +1,40 @@
+namespace DataCanVirtualBank{
+
+    public class ExchangeRate{
+        public required int _id { get; set; } //_currencyId
+
+        public required string _currency { get; set; }
+
+
+        //public float //_rateValue { get; set; }
+
+
+        //Getters
+        /*public string GetCurrencyId()
+        {
+            return _currencyId;
+        }
+        public float GetRateValue()
+        {
+            return _rateValue;
+        }
+        //Setters
+        public void SetCurrencyId(string currency)
+        {
+            _currencyId = currency;
+        }
+        public void SetRateValue(float rateValue)
+        {
+            _rateValue = rateValue;
+        }
+
+       // public ExchangeRate()
+       // {
+       // }
+        public ExchangeRate(string currency, float value)
+        {
+            this._currencyId = currency;
+            this._rateValue = value;
+        }*/
+    }
+}
